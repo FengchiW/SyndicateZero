8f1b8695e2d5b68bc88d7ef80d32e98352114e57
Wilson (Fengchi) Wang <wilsonfwang@gmail.com>
2020-03-24 19:17:01 -0400

    SyndicateZero's Big change

---
+++ main.js
@@ -0,0 +1,40 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const { app, BrowserWindow, Menu } = electron;
+
+let mainWindow;
+
+app.on('ready', function() {
+
+    mainWindow = new BrowserWindow({});
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate)
+
+    Menu.setApplicationMenu(mainMenu)
+});
+
+const menuTemplate = [{
+    label: 'File',
+    submenu: [{
+            label: 'Add',
+            click() {
+
+            }
+        },
+        {
+            label: 'Quit',
+            accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+            click() {
+                app.quit()
+            }
+        }
+    ]
+}]
